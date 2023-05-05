96480ea5e90c6ba604cbdbc18f293a75eeb89935
ChrisKnowles <christianjknowles@gmail.com>
2023-05-05 18:09:31 +0100

    MVP functioning as React App

--- weather-app/src/components/App/App.js
+++ weather-app/src/components/App/App.js
@@ -2,38 +2,39 @@ import logo from '../../logo.svg';
 import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import WeatherCard from '../WeatherCard/weatherCard';
 
-apiKey = `IA7w4QOpYihHr7eC23DrVBhm5OIcgIqh`
-
+let apiKey = `IA7w4QOpYihHr7eC23DrVBhm5OIcgIqh`
+let city = `London`
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
 
+  useEffect(() => {
+    async function getWeather() {
+      
+      // i would like to validate the input here so that it is a valid city name and not a number or something else
+      const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${city}&apikey=${apiKey}`)
+      const weatherData = await response.json()
+      console.log(weatherData)
+      setWeatherData(weatherData)
+    }
 
-  async function getWeather() {
-    const cityInput = document.getElementById("cityInput");
-    const city = cityInput.value;
-  
-    // i would like to validate the input here so that it is a valid city name and not a number or something else
-
-
-    const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${city}&apikey=${apiKey}`)
-    const weatherData = await response.json()
-    console.log(weatherData)
-
-   
-}
+    getWeather()
+  }, []);
 
 
   return (
     <div className="App">
       <p>Powered by <a href="https://www.tomorrow.io/weather-api">Tomorrow.io</a></p>
       <br></br>
-
-      <WeatherCard props = {props}/>
-      <Input handleSubmit={handleSubmit} data={data} setData={setData} />
-      
+      {weatherData && (
+      <WeatherCard weatherData = {weatherData}/>
+      )};
     </div>
   );
 };
 
 export default App;
+
+{/* <Input handleSubmit={handleSubmit} data={data} setData={setData} /> */}
--- weather-app/src/components/WeatherCard/weatherCard.js
+++ weather-app/src/components/WeatherCard/weatherCard.js
@@ -1,9 +1,15 @@
+import React from 'react'
+import './weatherCard.css'
 
 // need to pass JSON weather data as a prop
-function weatherCard (JSONDATA as a prop) { 
+function weatherCard ({weatherData}) { 
 
-// deconstruct JSONDATA prop into variables i want to use
-    const {title, text, img} = appProps
+    const maxTemp = weatherData.timelines.daily[0].values.temperatureMax
+    const minTemp = weatherData.timelines.daily[0].values.temperatureMin
+    const avgTemp = weatherData.timelines.daily[0].values.temperatureAvg
+    const weatherCodeMax = weatherData.timelines.daily[0].values.weatherCodeMax
+    const weatherCodeMin = weatherData.timelines.daily[0].values.weatherCodeMin
+        
 
     return (
         <div className="card">
@@ -11,11 +17,11 @@ function weatherCard (JSONDATA as a prop) {
             {/* <p>Enter a City, ZIP or Postcode: <input type="text" id="cityInput"/></p>
             <button onclick="getWeather()">Get Weather</button> */}
 
-            <p>Today's max temperature: <span id="maxTemp"></span></p>
-            <p>Today's min temperature: <span id="minTemp"></span></p>
-            <p>Today's average temperature: <span id="avgTemp"></span></p>
-            <p>Today's weather codeMax: <span id ="weatherCodeMax"></span></p>
-            <p>Today's weather codeMin: <span id ="weatherCodeMin"></span></p>
+            <p>Today's max temperature: {maxTemp}</p>
+            <p>Today's min temperature: {minTemp}</p>
+            <p>Today's average temperature: {avgTemp}</p>
+            <p>Today's weather codeMax: {weatherCodeMax}</p>
+            <p>Today's weather codeMin: {weatherCodeMin}</p>
             
             {/* not sure the button should be here or if it needs any props*/}
             
@@ -23,14 +29,10 @@ function weatherCard (JSONDATA as a prop) {
     )
 }
 
-export default Card
+export default weatherCard
 
 // this is the deconstruted data from vanilla js file
-// const maxTemp = weatherData.timelines.daily[0].values.temperatureMax
-// const minTemp = weatherData.timelines.daily[0].values.temperatureMin
-// const avgTemp = weatherData.timelines.daily[0].values.temperatureAvg
-// const weatherCodeMax = weatherData.timelines.daily[0].values.weatherCodeMax
-// const weatherCodeMin = weatherData.timelines.daily[0].values.weatherCodeMin
+
 
 // const maxTempElement = document.getElementById("maxTemp");
 // maxTempElement.textContent = maxTemp;
