93d5fc40572f073169a901dd8bc93ec06e038c63
ChrisKnowles <christianjknowles@gmail.com>
2023-05-07 08:01:43 +0100

    forgot to save these before the last commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 resolve: {
-  fallback: {
-    fs: false,
-    path: false,
-    os: false
-  }
+ fallback: {
+   fs: require.resolve("fs"),
+   path: require.resolve("path-browserify"),
+   os: require.resolve("os-browserify/browser")
+ }
 }
 
 
-
+// resolve: {
+//   fallback: {
+//     fs: false,
+//     path: false,
+//     os: false
+//   }
+// }
 
 
 
