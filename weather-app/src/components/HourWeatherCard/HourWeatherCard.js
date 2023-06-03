539b5e35f3f11df5c848dcab6e62df7d42e1fc4b
ChrisKnowles <christianjknowles@gmail.com>
2023-06-03 11:19:18 +0100

    refactored the HourWeatherCard code to display this hour and next hour and started to apply css to it

--- weather-app/src/components/HourWeatherCard/HourWeatherCard.js
+++ weather-app/src/components/HourWeatherCard/HourWeatherCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./HourWeather.css";
+import React, { useRef, useEffect } from "react";
+import "./HourWeatherCard.css";
 import ImportAll from "../Icons/icons";
 
 const images = ImportAll(
@@ -13,9 +13,9 @@ const images = ImportAll(
 // Have a button for next day so it takes the user to the next day
 // On next day display the current hour of today with previous hour and next hour with a carousel that goes both directions
 
-// then need to make this appear on a seperate page when the user clicks on the DetailWeatherCard
+// then need to make this appear on a separate page when the user clicks on the DetailWeatherCard
 
-export default function next24Hours({ weatherData }) {
+export default function Next24Hours({ weatherData }) {
   let now = new Date(); // Getting current date and time
   let nextDay = new Date(); // Getting the date and time 24 hours later
   nextDay.setHours(now.getHours() + 24);
@@ -26,11 +26,39 @@ export default function next24Hours({ weatherData }) {
     return itemDate >= now && itemDate <= nextDay; // Checking if item's time is between now and the next 24 hours
   });
 
-  console.log(hourlyData); // see below for hourlyData format
+  // Find the index of the current hour
+  const currentIndex = hourlyData.findIndex((item) => {
+    let itemDate = new Date(item.time);
+    return itemDate >= now;
+  });
 
+  // Get previous, current, and next hour data
+  const previousHour = hourlyData[currentIndex - 1];
+  const currentHour = hourlyData[currentIndex];
+  const nextHour = hourlyData[currentIndex + 1];
+
+  // Create a ref to the current hour div
+  const currentHourRef = useRef();
+
+  // Scroll to the current hour div when the component mounts
+  useEffect(() => {
+    currentHourRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  return (
+    <div className="HourWeatherCard">
+      {previousHour && <HourData data={previousHour} />}
+      {currentHour && <HourData data={currentHour} ref={currentHourRef} />}
+      {nextHour && <HourData data={nextHour} />}
+    </div>
+  );
+}
+
+// HourData is a new component for rendering each hour's data
+const HourData = React.forwardRef(({ data }, ref) => {
   let compassDirection = ""; //creating a block scope variable
   // Main Program
-  function compassDirectionConversion(hourlyData) {
+  function compassDirectionConversion(data) {
     const sector = [
       "N",
       "NNE",
@@ -50,28 +78,24 @@ export default function next24Hours({ weatherData }) {
       "NNW",
       "N",
     ]; // N can be 0deg or 360Deg so it is included twice in the array to capture this edge case.
-    let windDirection = hourlyData.values.windDirection;
-    compassDirection = sector[Math.round(windDirection / 22.5)]; // Rounds the wind direction out into 17 sectors. Sectors 0 and 16 are both N.  we could add more granularity by adding more sectors and changing the divisor according to howe many sectors we have chosen to use.
+    let windDirection = data.values.windDirection;
+    compassDirection = sector[Math.round(windDirection / 22.5)]; // Rounds the wind direction out into 17 sectors. Sectors 0 and 16 are both N.
     return compassDirection;
   }
 
   return (
-    <div>
-      {hourlyData.map((hourlyData, index) => (
-        <div key={index}>
-          <h3>Time: {hourlyData.time}</h3>
-          <h3>Temp: {hourlyData.values.temperature}</h3>
-          <h3>Humidity: {hourlyData.values.humidity}</h3>
-          <h3>Rain: {hourlyData.values.precipitationProbability}</h3>
-          <h3>Wind Speed: {hourlyData.values.windSpeed}</h3>
-          <h3>Wind Gust: {hourlyData.values.windGust}</h3>
-          <h3>Wind Direction: {hourlyData.values.windDirection}</h3>
-          <h3>Wind Compass: {compassDirectionConversion(hourlyData)}</h3>
-        </div>
-      ))}
+    <div ref={ref}>
+      <h3>Time: {data.time}</h3>
+      <h3>Temp: {data.values.temperature}</h3>
+      <h3>Humidity: {data.values.humidity}</h3>
+      <h3>Rain: {data.values.precipitationProbability}</h3>
+      <h3>Wind Speed: {data.values.windSpeed}</h3>
+      <h3>Wind Gust: {data.values.windGust}</h3>
+      <h3>Wind Direction: {data.values.windDirection}</h3>
+      <h3>Wind Compass: {compassDirectionConversion(data)}</h3>
     </div>
   );
-}
+});
 
 // hourly data is an array of arrays 0 to 24
 // each array is one hour in this format:
